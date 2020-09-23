4a31feaa6f3577ac26559aa80ffb18626938b95a
Mathter85 <70720278+Mathter85@users.noreply.github.com>
2020-09-23 19:03:09 +0300

    Add files via upload

--- script_vanilajs.js
+++ script_vanilajs.js
@@ -1,67 +1,71 @@
 var buttonNewTask = document.getElementsByClassName('antc-text-button')[0]; //получаем элемент Кнопка добавить дело, первый в коллеции
 var nameTask = document.getElementsByClassName('antc-text-nameNewTask')[0]; //получаем элемент поле с текстом для названия дела, первый в коллеции
-var descriptionTask = document.getElementsByClassName('antc-main-field-label-description')[0]; //получаем элемент поле с описание названия дела,  первый в коллеции
+var descriptionTask = document.getElementsByClassName('antc-text-descriptonNewTask')[0]; //получаем элемент поле с описание названия дела,  первый в коллеции
 
 buttonNewTask.onclick = function(){
   if ((nameTask.value !='') & (descriptionTask.value !=''))  //проверяем ввели ли название и описание
   {
-      var containerTasks = document.getElementsByClassName('list-tasks-column')[0];//new 22 09 2020
-      var ltcHeader = document.getElementsByClassName('ltc-header')[0]; // new 22 09 2020
+      var containerTasks = document.getElementsByClassName('list-tasks-column')[0];
+      var ltcHeader = document.getElementsByClassName('ltc-header')[0];
 
-      var listNameTasks = containerTasks.querySelectorAll('.ltc-nameTask'); // new 22 09 2020
-      var listDescritionTasks = containerTasks.querySelectorAll('.ltc-descriptionTask'); // new 22 09 2020
-
+      var listNameTasks = containerTasks.querySelectorAll('.ltc-nameTask');
+      var listDescritionTasks = containerTasks.querySelectorAll('.ltc-descriptionTask');
       if (listNameTasks.length==0){  // нет задач
           var emptyBlock = document.getElementsByClassName('ltc-emptylist')[0];
           emptyBlock.remove();
           ltcHeader.insertAdjacentHTML('afterend',
+          '<div class="taskContainer">'+
             '<div class="ltc-task">' +
               '<div class="ltc-nameTask">'+ nameTask.value +'</div>'+
-              '<button class="button-deleteTask" type="button" name="deleteTask"><img src="img/del-button.png" alt="delete"></button>'+
-              '<button class="button-rowDown" type="button" name="rowDown"><img src="img/rect-down.png" alt="hide"></button>'+
+              '<button class="button-deleteTask" type="button" name="deleteTask"></button>'+
+              '<button class="button-rowDown" type="button" name="rowDown"></button>'+
             '</div>'+
             '<div class="ltc-descriptionTask">'+
               descriptionTask.value +
-            '</div>');
+            '</div>'+
+          '</div>');
 
           } else{  //есть задачи
               //берем последнюю задачу и добавлеям новую в конец
               listDescritionTasks[listNameTasks.length-1].insertAdjacentHTML('afterend',
+              '<div class="taskContainer">'+
                 '<div class="ltc-task">' +
                   '<div class="ltc-nameTask">'+ nameTask.value +'</div>'+
-                  '<button class="button-deleteTask" type="button" name="deleteTask"><img src="img/del-button.png" alt="delete"></button>'+
-                  '<button class="button-rowDown" type="button" name="rowDown"><img src="img/rect-down.png" alt="hide"></button>'+
+                  '<button class="button-deleteTask" type="button" name="deleteTask"></button>'+
+                  '<button class="button-rowDown" type="button" name="rowDown"></button>'+
                 '</div>'+
                 '<div class="ltc-descriptionTask">'+
                   descriptionTask.value +
-                '</div>');
+                '</div>'+
+              '</div>');
           }
-
-
-
-
     }
     else {
           alert('Не заполнены все обязательные поля!');
     }
-//     //удаляем Список пуст
-//     $('.ltc-emptylist').remove();
-//
-//     //Добавляем задачу
-// //     $('.list-tasks-column').append(
-//       '<div class="ltc-task">'+
-//         '<div class="ltc-nameTask">'+ $('.antc-text-nameNewTask').val() +'</div>'+
-//         '<button class="button-deleteTask" type="button" name="deleteTask"><img src="img/del-button.png" alt="delete"></button>'+
-//         '<button class="button-rowDown" type="button" name="rowDown"><img src="img/rect-down.png" alt="hide"></button>'+
-//       '</div>'+
-//       '<div class="ltc-descriptionTask">'+
-//         $('.antc-text-descriptonNewTask').val()+
-//       '</div>');
-//
-//     } else {
-//       alert('Не заполнены все обязательные поля!');
-//     }
-// });
-
-
 };
+
+
+
+document.body.addEventListener("click", function(event) { //ловим все нажатия кнопки
+  if (event.target.classList.contains('button-deleteTask')){ //нажата кнопка удалить
+      event.target.parentNode.nextSibling.remove();//удаляем описание задачи
+      event.target.parentNode.remove();//удаляем задачу
+
+      var containerTasks = document.getElementsByClassName('list-tasks-column')[0];
+      var listNameTasks = containerTasks.querySelectorAll('.ltc-nameTask');
+      if (listNameTasks.length==0){//последняя задача
+        var ltcHeader = document.getElementsByClassName('ltc-header')[0];
+        ltcHeader.insertAdjacentHTML('afterend',//добавляем слова Список пуст
+            '<div class="ltc-emptylist">Список пуст...</div>');
+      }
+  }
+
+  // var listNameTasks = containerTasks.querySelectorAll('.ltc-nameTask');
+  if (event.target.classList.contains('button-rowDown')){
+      event.target.parentNode.nextSibling.classList.toggle("hide-block");
+      event.target.parentNode.classList.toggle("add-marginbottom-20px");
+      event.target.classList.toggle('add-backgroundimage-rect-down-right');
+
+  }
+});
